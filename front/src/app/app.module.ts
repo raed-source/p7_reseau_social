62fbc8129047599752da885d0efe2c6da7c00916
raed ABBAS <raed.abbas@hotmail.fr>
2022-10-21 15:12:59 +0200

    auth, signup, authservice

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { PostComponent } from './post/post.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { LogInComponent } from './auth/log-in/log-in.component';
+import { HomeComponent } from './home/home.component';
+import { Post } from './models/post-model';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostComponent,
+    SignUpComponent,
+    LogInComponent,
+    HomeComponent,
+    Post
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
